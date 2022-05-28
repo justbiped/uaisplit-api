d7512a0fce65c494fb93929cc70494c3cc152a07
Roubert Edgar <roubertedgar@gmail.com>
2022-05-28 19:38:36 -0300

    Move to spring boot

---
+++ src/test/kotlin/com/biped/tosplit/ToSplitApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.biped.tosplit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToSplitApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
