ad855ec36365e919777bc24e1d5ebfa219030d81
Roubert Edgar <roubertedgar@gmail.com>
2022-05-28 22:35:18 -0300

    Receive web request in the exception hadler advice class

--- application/src/main/kotlin/com/biped/tosplit/config/ExceptionHandlerAdvice.kt
+++ application/src/main/kotlin/com/biped/tosplit/config/ExceptionHandlerAdvice.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.context.request.WebRequest
 
 @ControllerAdvice
 class ExceptionHandlerAdvice {
@@ -12,7 +13,7 @@ class ExceptionHandlerAdvice {
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    fun handleIllegalArguments(error: Throwable): String? {
+    fun handleIllegalArguments(error: Throwable, request: WebRequest): String? {
         //log something here
         return error.message
     }
