1006b56eeb3845461882af25a38cee176bbced93
Roubert Edgar <roubertedgar@gmail.com>
2022-05-28 22:32:19 -0300

    Change the controller advice name

--- application/src/main/kotlin/com/biped/tosplit/config/ExceptionHandlerAdvice.kt
+++ application/src/main/kotlin/com/biped/tosplit/config/ExceptionHandlerAdvice.kt
@@ -1,13 +1,13 @@
 package com.biped.tosplit.config
 
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestControllerAdvice
-class ControllerAdvice {
+@ControllerAdvice
+class ExceptionHandlerAdvice {
 
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
