11e453f21381109a962c6b6337b2c66adb4aec4f
Roubert Edgar <roubertedgar@gmail.com>
2023-01-27 02:10:58 -0300

    Remove star imports from transaction controller

--- application/src/main/kotlin/biped/works/tosplit/transaction/controller/TransactionController.kt
+++ application/src/main/kotlin/biped/works/tosplit/transaction/controller/TransactionController.kt
@@ -5,8 +5,11 @@ import biped.works.tosplit.transaction.data.TransactionMetadataRepository
 import biped.works.tosplit.transaction.data.TransactionRequest
 import biped.works.tosplit.transaction.data.toDomain
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/transaction")
