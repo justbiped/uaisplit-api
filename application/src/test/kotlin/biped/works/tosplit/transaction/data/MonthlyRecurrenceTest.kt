a486fd686986c0382863877d7e0691365aab136b
Roubert Edgar <roubertedgar@gmail.com>
2024-02-19 19:13:20 -0300

    Reformat montly recurrence test

--- application/src/test/kotlin/biped/works/tosplit/transaction/data/MonthlyRecurrenceTest.kt
+++ application/src/test/kotlin/biped/works/tosplit/transaction/data/MonthlyRecurrenceTest.kt
@@ -8,6 +8,27 @@ import java.time.LocalDate
 class MonthlyRecurrenceTest {
 
     @Test
+    fun `conclusion is set to max value when frequency times is less than 1`() {
+        val recurrence = MonthlyRecurrence(
+            recurrence(start = LocalDate.parse("2024-01-10"), frequency = "times=-1")
+        )
+
+        val conclusion = recurrence.conclusion
+
+        assertThat(conclusion == LocalDate.MAX)
+    }
+
+    @Test
+    fun `conclusion is equals the start date when frequency times is 1`() {
+        val start = LocalDate.parse("2024-01-10")
+        val recurrence = MonthlyRecurrence(recurrence(start = start, frequency = "times=1"))
+
+        val conclusion = recurrence.conclusion
+
+        assertThat(conclusion).isEqualTo(start)
+    }
+
+    @Test
     fun `returns 01-10 date for one time recurrence matching time span 01-10 to infinity`() {
         val recurrence = recurrence(
             start = LocalDate.parse("2024-01-10"),
@@ -76,31 +97,5 @@ class MonthlyRecurrenceTest {
             LocalDate.parse("2024-01-10"),
             LocalDate.parse("2024-02-10"),
         )
-    }
-
-    @Test
-    fun `conclusion is set to max value when frequency times is less than 1`() {
-        val recurrence = MonthlyRecurrence(
-            recurrence(
-                start = LocalDate.parse("2024-01-10"),
-                frequency = "times=-1"
-            )
-        )
-
-        val conclusion = recurrence.conclusion
-
-        assertThat(conclusion == LocalDate.MAX)
-    }
-
-    @Test
-    fun `conclusion is equals the start date when frequency times is 1`() {
-        val start = LocalDate.parse("2024-01-10")
-        val recurrence = MonthlyRecurrence(
-            recurrence(start = start, frequency = "times=1")
-        )
-
-        val conclusion = recurrence.conclusion
-
-        assertThat(conclusion == start)
     }
 }
